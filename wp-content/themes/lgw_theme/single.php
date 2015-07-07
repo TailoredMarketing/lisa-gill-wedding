d1def75d5d4fe78122954cb608d8f1b4e9cfdd91
@dantaylorseo <dan@tailored.im>
2015-07-07 19:39:10 +0100

    theme updates

--- wp-content/themes/lgw_theme/single.php
+++ wp-content/themes/lgw_theme/single.php
@@ -5,10 +5,13 @@
 				while( have_posts() ) : 
 					the_post();
 			?>
-        	<div class="col-md-24 ">
+        	<div class="col-md-24">
             	<?php 
 					if ( has_post_thumbnail() ) {
-						the_post_thumbnail( 'full', array( 'class' => 'img-responsive' ) ); 
+						echo '<div class="has_thumb">';
+							the_post_thumbnail( 'full', array( 'class' => 'img-responsive' ) ); 
+							echo '<h1>'.get_the_title().'</h1>';
+						echo '</div>';
 					}  
 				?>
             </div>
