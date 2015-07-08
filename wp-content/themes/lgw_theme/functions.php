b4bb004dae63ef28aa4102689a1298fb0e83a84f
@dantaylorseo <dan@tailored.im>
2015-07-08 16:07:30 +0100

    theme additions and plugin add

--- wp-content/themes/lgw_theme/functions.php
+++ wp-content/themes/lgw_theme/functions.php
@@ -87,7 +87,7 @@ class tailored_theme_class {
 	}
     
     public function register_widgets (){
-
+		register_widget( 'social_widget' );
 	}
 	
 	public function register_post_types() {
--- wp-content/themes/lgw_theme/inc/widgets.php
+++ wp-content/themes/lgw_theme/inc/widgets.php
@@ -1,2 +1,32 @@
 <?php
-	   
+	   class social_widget extends WP_Widget {
+
+		public function __construct() {
+			parent::__construct(
+				'social_widget', // Base ID
+				__('Social Icons', 'text_domain'), // Name
+				array( 'description' => __( 'Shows the social icons', 'text_domain' ), ) // Args
+			);
+		}
+	
+		public function widget( $args, $instance ) { ?>
+			<?php echo $args['before_widget']; ?>
+			<p class="text-center social-icons">
+            	<a href="http://twitter.com/LisaGillPhoto"><i class="fa fa-twitter fa-2x"></i></a> 
+                <a href="http://www.facebook.com/LisaGillPhotographyStudio"><i class="fa fa-facebook fa-2x"></i></a> 
+                <a href="https://pinterest.com/lisagillphoto/"><i class="fa fa-pinterest fa-2x"></i></a>
+            </p>            
+            <?php echo $args['after_widget']; ?>
+        <?php
+		}
+	
+		public function form( $instance ) {
+		?>
+            <p>There are no options for this widget.</p>
+        <?php
+		}
+	
+		public function update( $new_instance, $old_instance ) {
+			
+		}
+	}
