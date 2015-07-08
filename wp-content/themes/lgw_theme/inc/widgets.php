4bc636f74a291f18d30f1d0b2545724a2ac8658f
@dantaylorseo <dan@tailored.im>
2015-07-08 16:10:37 +0100

    theme additions and plugin add

--- wp-content/themes/lgw_theme/inc/widgets.php
+++ wp-content/themes/lgw_theme/inc/widgets.php
@@ -1,32 +1,68 @@
 <?php
-	   class social_widget extends WP_Widget {
+class social_widget extends WP_Widget {
+
+	public function __construct() {
+		parent::__construct(
+			'social_widget', // Base ID
+			__('Social Icons', 'text_domain'), // Name
+			array( 'description' => __( 'Shows the social icons', 'text_domain' ), ) // Args
+		);
+	}
+	
+	public function widget( $args, $instance ) { ?>
+		<?php echo $args['before_widget']; ?>
+		<p class="text-center social-icons">
+			<a href="http://twitter.com/LisaGillPhoto"><i class="fa fa-twitter fa-2x"></i></a> 
+			<a href="http://www.facebook.com/LisaGillPhotographyStudio"><i class="fa fa-facebook fa-2x"></i></a> 
+			<a href="https://pinterest.com/lisagillphoto/"><i class="fa fa-pinterest fa-2x"></i></a>
+		</p>            
+		<?php echo $args['after_widget']; ?>
+	<?php
+	}
+	
+	public function form( $instance ) {
+	?>
+		<p>There are no options for this widget.</p>
+	<?php
+	}
+	
+	public function update( $new_instance, $old_instance ) {
+		
+	}
+}
+class contact_widget extends WP_Widget {
 
 		public function __construct() {
 			parent::__construct(
-				'social_widget', // Base ID
-				__('Social Icons', 'text_domain'), // Name
-				array( 'description' => __( 'Shows the social icons', 'text_domain' ), ) // Args
+				'tim_featured_allotments', // Base ID
+				__('Featured Allotments', 'text_domain'), // Name
+				array( 'description' => __( 'Shows the Featured Allotments Section', 'text_domain' ), ) // Args
 			);
 		}
 	
 		public function widget( $args, $instance ) { ?>
 			<?php echo $args['before_widget']; ?>
-			<p class="text-center social-icons">
-            	<a href="http://twitter.com/LisaGillPhoto"><i class="fa fa-twitter fa-2x"></i></a> 
-                <a href="http://www.facebook.com/LisaGillPhotographyStudio"><i class="fa fa-facebook fa-2x"></i></a> 
-                <a href="https://pinterest.com/lisagillphoto/"><i class="fa fa-pinterest fa-2x"></i></a>
-            </p>            
+            <p class="text-center">For more information, please<br>
+                do email or give me a call:</p>
+                <p class="text-center"><strong><?php echo $instance['phone']; ?></strong></p>
+                <hr>
+                <p class="text-center">I am also on Facebook, Twitter<br>
+                and more recently Linked In.</p>
             <?php echo $args['after_widget']; ?>
         <?php
 		}
 	
 		public function form( $instance ) {
-		?>
-            <p>There are no options for this widget.</p>
+			$instance = wp_parse_args( (array) $instance, array( 'phone' => '(01494) 680811' ) );
+            $title = $instance['phone'];
+?>
+            <p><label for="<?php echo $this->get_field_id('phone'); ?>">Phone Number: <input class="widefat" id="<?php echo $this->get_field_id('phone'); ?>" name="<?php echo $this->get_field_name('phone'); ?>" type="text" value="<?php echo esc_attr($phone); ?>" /></label></p>
         <?php
 		}
 	
 		public function update( $new_instance, $old_instance ) {
-			
+			$instance = $old_instance;
+            $instance['phone'] = $new_instance['phone'];
+            return $instance;
 		}
 	}
